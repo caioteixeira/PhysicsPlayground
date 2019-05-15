cb946ac967b8d7d3d88bc1b46ab054e4b9e4df08
Caio Teixeira <caiovmteixeira@outlook.com>
2019-05-14 23:06:56 -0300

    Remove PhysX references

--- src/simulationWorld.cpp
+++ src/simulationWorld.cpp
@@ -1,20 +1,5 @@
 #include "simulationWorld.h"
 
-#include "PxPhysicsAPI.h"
-
-physx::PxDefaultAllocator		gAllocator;
-physx::PxDefaultErrorCallback	gErrorCallback;
-
-physx::PxFoundation*			gFoundation = NULL;
-physx::PxPhysics*				gPhysics = NULL;
-
-physx::PxDefaultCpuDispatcher*	gDispatcher = NULL;
-physx::PxScene*				gScene = NULL;
-
-physx::PxMaterial*				gMaterial = NULL;
-
-physx::PxPvd*                  gPvd = NULL;
-
 SimulationWorld::SimulationWorld() 
 {
     const auto mesh = graphics::createCubeMesh();
@@ -26,14 +11,6 @@ SimulationWorld::SimulationWorld()
     cube2.mesh = mesh;
     cube2.position = bx::Vec3(0, -3, 0);
     cube2.scale = bx::Vec3(5, 0.3f, 5);
-
-    physx::PxShape* shape = gPhysics->createShape(physx::PxBoxGeometry(0.5, 0.5, 0.5), *gMaterial);
-    
-    physx::PxTransform localTm(physx::PxVec3(physx::PxReal(0) - physx::PxReal(0), physx::PxReal(0), 0));
-    physx::PxRigidDynamic* body = gPhysics->createRigidDynamic(localTm);
-    body->attachShape(*shape);
-    physx::PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
-    gScene->addActor(*body);
 
     mElements.push_back(cube1);
     mElements.push_back(cube2);
@@ -54,32 +31,5 @@ void SimulationWorld::render()
 
 void SimulationWorld::initPhysics()
 {
-    using namespace physx;
-
-     gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
     
-     gPvd = PxCreatePvd(*gFoundation);
-     PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
-     gPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
-    
-     gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), true, gPvd);
-    
-     PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
-     sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
-     gDispatcher = PxDefaultCpuDispatcherCreate(2);
-     sceneDesc.cpuDispatcher = gDispatcher;
-     sceneDesc.filterShader = PxDefaultSimulationFilterShader;
-     gScene = gPhysics->createScene(sceneDesc);
-    
-     PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
-     if (pvdClient)
-     {
-         pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
-         pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
-         pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
-     }
-     gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
-    
-     physx::PxRigidStatic* groundPlane = PxCreatePlane(*gPhysics, physx::PxPlane(0, 1, 0, 0), *gMaterial);
-     gScene->addActor(*groundPlane);
 }
