81e9308b634741348fd2d1505fe85adb4c39673a
Caio Teixeira <caiovmteixeira@outlook.com>
2019-05-19 20:55:46 -0300

    Compile Shaders from CMake

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -116,27 +116,7 @@ bgfx::ShaderHandle graphics::loadShader(const char *FILENAME)
 {
     char filePath[512];
 
-    const char* shaderPath = "???";
-
-    switch (bgfx::getRendererType())
-    {
-    case bgfx::RendererType::Noop:
-    case bgfx::RendererType::Direct3D9:  shaderPath = "shaders/dx9/";   break;
-    case bgfx::RendererType::Direct3D11:
-    case bgfx::RendererType::Direct3D12: shaderPath = "shaders/dx11/";  break;
-    case bgfx::RendererType::Gnm:        shaderPath = "shaders/pssl/";  break;
-    case bgfx::RendererType::Metal:      shaderPath = "shaders/metal/"; break;
-    case bgfx::RendererType::Nvn:        shaderPath = "shaders/nvn/";   break;
-    case bgfx::RendererType::OpenGL:     shaderPath = "shaders/glsl/";  break;
-    case bgfx::RendererType::OpenGLES:   shaderPath = "shaders/essl/";  break;
-    case bgfx::RendererType::Vulkan:     shaderPath = "shaders/spirv/"; break;
-
-    case bgfx::RendererType::Count:
-        BX_CHECK(false, "You should not be here!");
-        break;
-    }
-
-    bx::strCopy(filePath, BX_COUNTOF(filePath), shaderPath);
+    bx::strCopy(filePath, BX_COUNTOF(filePath), "shaders/");
     bx::strCat(filePath, BX_COUNTOF(filePath), FILENAME);
     bx::strCat(filePath, BX_COUNTOF(filePath), ".bin");
 
